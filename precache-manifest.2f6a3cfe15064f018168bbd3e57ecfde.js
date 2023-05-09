313d654b5eaea761043b1fae874d4d69227f05d5
Rimsha <rimshanaeem808@gmail.com>
2023-05-09 02:06:28 -0700

    Updates

---
+++ precache-manifest.2f6a3cfe15064f018168bbd3e57ecfde.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8679f1072a4ee295869",
+    "url": "/2todolist/static/css/main.0b16b36e.chunk.css"
+  },
+  {
+    "revision": "a8679f1072a4ee295869",
+    "url": "/2todolist/static/js/main.a8679f10.chunk.js"
+  },
+  {
+    "revision": "f72be3c5c63d8985eb10",
+    "url": "/2todolist/static/js/1.f72be3c5.chunk.js"
+  },
+  {
+    "revision": "e7b85c1f2d7850ddbb39",
+    "url": "/2todolist/static/js/runtime~main.e7b85c1f.js"
+  },
+  {
+    "revision": "e5fc0f3058fdab77bf7b025fc7633cef",
+    "url": "/2todolist/index.html"
+  }
+];
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import  ReactDOM  from "react-dom";
-import "./index.css";
-import ToDoList from "./ToDoList";
-
-ReactDOM.render(<ToDoList />, document.getElementById("root"));
